cca58de9349550366e82bc10df8ac2572e8b45de
Uniskela <104075208+uniskela@users.noreply.github.com>
2026-08-22 23:02:13 +1000

    test: cover legacy bin and consent migrations

--- Jellyfin.Plugin.MediaDash.Tests/PluginConfigurationPrivacyTests.cs
+++ Jellyfin.Plugin.MediaDash.Tests/PluginConfigurationPrivacyTests.cs
@@ -26,15 +26,32 @@ public class PluginConfigurationPrivacyTests
         Assert.True(configuration.NormalizeAnalyticsConsent());
         Assert.False(configuration.AnalyticsEnabled);
         Assert.Equal(string.Empty, configuration.AnalyticsInstallId);
+        Assert.Equal(1, configuration.AnalyticsConsentVersion);
     }
 
     [Fact]
-    public void ExplicitAnalyticsConsentWithInstallIdIsPreserved()
+    public void LegacyAutomaticallyMintedInstallIdIsNotTreatedAsConsent()
     {
         var configuration = new PluginConfiguration
         {
             AnalyticsEnabled = true,
             AnalyticsInstallId = "54834b32-926e-4f79-a39a-741d6fcad224"
+        };
+
+        Assert.True(configuration.NormalizeAnalyticsConsent());
+        Assert.False(configuration.AnalyticsEnabled);
+        Assert.Equal(string.Empty, configuration.AnalyticsInstallId);
+        Assert.Equal(1, configuration.AnalyticsConsentVersion);
+    }
+
+    [Fact]
+    public void VersionedExplicitAnalyticsConsentIsPreserved()
+    {
+        var configuration = new PluginConfiguration
+        {
+            AnalyticsEnabled = true,
+            AnalyticsInstallId = "54834b32-926e-4f79-a39a-741d6fcad224",
+            AnalyticsConsentVersion = 1
         };
 
         Assert.False(configuration.NormalizeAnalyticsConsent());
